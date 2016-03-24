45d6f150ed1a8167df4d13f84fee223bd9357903
Jimmy Pocock <jimmypocock@yahoo.com>
2016-03-23 21:43:12 -0500

    Mobilized

--- scripts/home.js
+++ scripts/home.js
@@ -19,20 +19,22 @@ $(function () {
     fullscreen();
   });
 
-  $('.artwork-wrapper').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.artwork-nav'
-  });
-  $('.artwork-nav').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    asNavFor: '.artwork-wrapper',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true
-  });
+  if ($(window).width() > 767) {
+    $('.artwork-wrapper').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.artwork-nav'
+    });
+    $('.artwork-nav').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      asNavFor: '.artwork-wrapper',
+      dots: true,
+      centerMode: true,
+      focusOnSelect: true
+    });
+  }
 
 });
